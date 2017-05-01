62f71da0d49367404888e058eda55f075d2766ed
Indra Basak <indra.basak1@gmail.com>
2017-04-30 22:03:08 -0700

    Removed book table.

--- service/src/main/resources/db/create-db.sql
+++ service/src/main/resources/db/create-db.sql
@@ -1,14 +1,3 @@
-CREATE TABLE books
-(
-  id                    BINARY(16) PRIMARY KEY,
-  title                 VARCHAR(32) NOT NULL,
-  genre                 VARCHAR(32) NOT NULL,
-  publisher             VARCHAR(32) NOT NULL,
-  star                  INTEGER,
-  author_first_name     VARCHAR(32) NOT NULL,
-  author_last_name      VARCHAR(32) NOT NULL
-);
-
 CREATE TABLE menagerie
 (
   id                    BINARY(16) PRIMARY KEY,
