d06cd6a5205f5d16ba710af2416e358c45952ae4
Indra Basak <indra.basak1@gmail.com>
2017-05-02 10:32:38 -0700

    Updated Api Listing Scanner plugin example.

--- service/src/main/java/com/basaki/example/menagerie/swagger/plugin/ExampleApiListingScanner.java
+++ service/src/main/java/com/basaki/example/menagerie/swagger/plugin/ExampleApiListingScanner.java
@@ -12,6 +12,8 @@ import springfox.documentation.builders.OperationBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiDescription;
+import springfox.documentation.service.Operation;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.ApiListingScannerPlugin;
 import springfox.documentation.spi.service.contexts.DocumentationContext;
@@ -26,49 +28,51 @@ import springfox.documentation.swagger.common.SwaggerPluginSupport;
 @Slf4j
 public class ExampleApiListingScanner implements ApiListingScannerPlugin {
 
-    // tag::api-listing-plugin[]
     @Override
     public List<ApiDescription> apply(DocumentationContext context) {
-        System.out.println("*************************");
-        return new ArrayList<ApiDescription>(
-                Arrays.asList( //<1>
-                        new ApiDescription(
-                                "/elephants/1767",
-                                "This is a bug",
-                                Arrays.asList( //<2>
-                                        new OperationBuilder(
-                                                new CachingOperationNameGenerator())
-                                                .authorizations(new ArrayList())
-                                                .codegenMethodNameStem(
-                                                        "bug1767GET") //<3>
-                                                .method(HttpMethod.GET)
-                                                .notes("This is a test method")
-                                                .parameters(
-                                                        Arrays.asList( //<4>
-                                                                new ParameterBuilder()
-                                                                        .description(
-                                                                                "search by description")
-                                                                        .type(new TypeResolver().resolve(
-                                                                                String.class))
-                                                                        .name("description")
-                                                                        .parameterType(
-                                                                                "query")
-                                                                        .parameterAccess(
-                                                                                "access")
-                                                                        .required(
-                                                                                true)
-                                                                        .modelRef(
-                                                                                new ModelRef(
-                                                                                        "string")) //<5>
-                                                                        .build()))
-                                                .build()),
-                                false)));
+        List<ApiDescription> descps = new ArrayList<>();
+        descps.add(createApiDescription());
+
+        return descps;
     }
-    // tag::api-listing-plugin[]
 
     @Override
     public boolean supports(DocumentationType documentationType) {
         //return DocumentationType.SWAGGER_2.equals(documentationType);
         return true;
+    }
+
+    private ApiDescription createApiDescription() {
+        ApiDescription desc = new ApiDescription(
+                "/elephants/children",
+                "This is an AP Listing Scanner example ",
+                Arrays.asList(createOperation()),
+                false);
+
+        return desc;
+    }
+
+    private Operation createOperation() {
+        ParameterBuilder paramBldr = new ParameterBuilder();
+        paramBldr.description("search by child's name")
+                .type(new TypeResolver().resolve(String.class))
+                .name("name")
+                .parameterType("query")
+                .parameterAccess("access")
+                .required(true)
+                .modelRef(new ModelRef("string"));
+        Parameter param = paramBldr.build();
+
+        OperationBuilder bldr =
+                new OperationBuilder(new CachingOperationNameGenerator());
+        bldr.authorizations(new ArrayList())
+                .codegenMethodNameStem("readChildrenGET")
+                .method(HttpMethod.GET)
+                .notes("This is a dynamic method to fetch elephant children")
+                .parameters(Arrays.asList(param));
+
+        Operation op = bldr.build();
+
+        return op;
     }
 }
